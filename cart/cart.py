4e73287b3d8c19a4562f886c8fe90597f7db5d4a
Denis Budin <D20041152@yandex.ru>
2024-06-20 19:38:06 +0300

    added coupons

--- cart/cart.py
+++ cart/cart.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart:
@@ -9,7 +10,27 @@ class Cart:
         cart = self.session.get(settings.CART_SESSION_ID)
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
-        self.cart = cart    
+        self.cart = cart  
+        self.coupon_id = self.session.get("coupon_id")  
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+        return None
+        
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) * self.get_total_price()
+        return Decimal(0)
+    
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
+
 
     def save(self):
             # пометить сеанс как "измененный",
